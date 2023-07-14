84d935293b4316a4e95898da4c1acea309bd12c0
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-07-14 12:26:33 -0300

    :recycle: refactor: Refatoração do componente Card para usar o hook useCard

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,108 +1,69 @@
-import { useContext, useEffect, useState } from 'react';
-import { BsFillHeartFill, BsStarFill }  from 'react-icons/bs';
+import { useState } from 'react';
 import { motion } from 'framer-motion';
-// Custom Hooks
-import useFavorites from 'hooks/useFavorites';
-import useRating from 'hooks/useRating';
-// Context
-import { useAppContext } from 'context/useAppContext';
-import { AuthContext } from 'context/AuthContext';
-// Styles
-import './card.scss';
-// Components
+import { BsFillHeartFill, BsStarFill } from 'react-icons/bs';
+import useCard from 'hooks/useCard';
 import Modal from 'components/Modal/Modal';
+import './card.scss';
 
 const Card = ({ title, thumbnailUrl, item, developer }) => {
-  const { user } = useContext(AuthContext)
-  const { favorites, setFavorites, ratings } = useAppContext();
-  const isFavorited = favorites.some((favorite) => favorite.id === item.id);
-  const [ isFavorite, setIsFavorite ] = useState(isFavorited);
-  const { handleAddFavorite, handleRemoveFavorite }  = useFavorites(setFavorites);
-  const [ isRating, setIsRating ] = useState(ratings[item.id] || 0);
-  const { handleAddRatings, handleRemoveRatings } = useRating(item);
-  const [ highlightStar, setHighlightStar ] = useState(-1);
-  const [ showModal, setShowModal ] = useState(false);
-  const [ textMessage, setTextMessage ] = useState('');
+  const [highlightStar, setHighlightStar] = useState(-1);
+  const {
+    isFavorite,
+    isRating,
+    showModal,
+    setShowModal,
+    textMessage,
+    handleFavoritClick,
+    handleRatingClick,
+  } = useCard(item);
 
-  useEffect(() => {
-    if(!user) {
-      setIsFavorite(false);
-      setIsRating(0);
-    }
-  }, [user])
-
-  // Função para itens favoritos
-  const handleFavoritClick = () => {
-    if (!user) {
-      setShowModal(true);
-      setTextMessage("adicionar como favorito")
-      return;
-    }
-    setIsFavorite(!isFavorite)
-    if(isFavorite) {
-      handleRemoveFavorite(item);
-    } else {
-      handleAddFavorite(item);
-    }
-  }
-
-  //Função para itens avaliados
-  const handleRatingClick = (newRating) => {
-    if (!user) {
-      setShowModal(true);
-      setTextMessage("avaliar o jogo")
-      return;
-    }
-    if(ratings[item.id] === newRating) {
-      handleRemoveRatings(item);
-      setIsRating(0);
-    } else {
-      handleAddRatings({...item, rating: newRating});
-      setIsRating(newRating);
-    }
-  };
-
-  // Para animação das estrelas de avaliação
+  // Funções para lidar com a animação das estrelas 
   const handleMouseEnter = (index) => {
-    setHighlightStar(index)
-  }
+    setHighlightStar(index);
+  };
   const handleMouseLeave = () => {
     setHighlightStar(-1);
-  }
+  };
 
   return (
     <>
-      <Modal 
+      <Modal
         showModal={showModal}
-        setShowModal={setShowModal} 
+        setShowModal={setShowModal}
         textMessage={textMessage}
       />
       <motion.div
-        layout 
-        animate={{ opacity: 1.5, scale: 1 }} 
-        initial={{ opacity: 1, scale: 0.8 }} 
+        layout
+        animate={{ opacity: 1.5, scale: 1 }}
+        initial={{ opacity: 1, scale: 0.8 }}
         exit={{ opacity: 1, scale: 0.8 }}
-        transition={{duration: .6}} 
+        transition={{ duration: 0.6 }}
         className='card'
       >
         <div className='card-wrapper'>
-          <img src={thumbnailUrl} alt={title} className='card-wrapper-img'/>
-        </div> 
+          <img src={thumbnailUrl} alt={title} className='card-wrapper-img' />
+        </div>
         <div className='card-content'>
           <h2 className='card-content-title'>{title}</h2>
-          <p className='card-content-dev'>Developer: <span>{developer}</span></p>
+          <p className='card-content-dev'>
+            Developer: <span>{developer}</span>
+          </p>
           <div className='card-content-rating'>
-            <BsFillHeartFill 
-              onClick={ handleFavoritClick }
+            <BsFillHeartFill
+              onClick={handleFavoritClick}
               className={isFavorite ? 'isFavorite' : ''}
             />
             <div onMouseLeave={handleMouseLeave}>
               {[1, 2, 3, 4].map((star, index) => (
-                <BsStarFill 
+                <BsStarFill
                   key={star}
-                  onClick={ () => handleRatingClick(star) }
+                  onClick={() => handleRatingClick(star)}
                   onMouseEnter={() => handleMouseEnter(index)}
-                  className={(isRating >= star || highlightStar >= index) ? 'highlight' : ''}
+                  className={
+                    isRating >= star || highlightStar >= index
+                      ? 'highlight'
+                      : ''
+                  }
                 />
               ))}
             </div>
---
+++ src/hooks/useCard.jsx
@@ -0,0 +1,73 @@
+import { useContext, useEffect, useState } from 'react';
+import { useAppContext } from 'context/useAppContext';
+import { AuthContext } from 'context/AuthContext';
+import useFavorites from 'hooks/useFavorites';
+import useRating from 'hooks/useRating';
+
+const useCard = (item) => {
+  const { user } = useContext(AuthContext);
+  const { favorites, setFavorites, ratings } = useAppContext();
+
+  const isFavorited = favorites.some((favorite) => favorite.id === item.id);
+  const [isFavorite, setIsFavorite] = useState(isFavorited);
+
+  const [isRating, setIsRating] = useState(ratings[item.id] || 0);
+
+  const [showModal, setShowModal] = useState(false);
+  const [textMessage, setTextMessage] = useState('');
+  
+  const { handleAddFavorite, handleRemoveFavorite } = useFavorites(setFavorites);
+  const { handleAddRatings, handleRemoveRatings } = useRating(item);
+
+  // Limpa marcação de favoritos e avaliações quando o usuário não está logado
+  useEffect(() => {
+    if (!user) {
+      setIsFavorite(false);
+      setIsRating(0);
+    }
+  }, [user]);
+
+  // Função para lidar com cliques em itens favoritos
+  const handleFavoritClick = () => {
+    if (!user) {
+      setShowModal(true);
+      setTextMessage('adicionar como favorito');
+      return;
+    }
+    setIsFavorite(!isFavorite);
+    if (isFavorite) {
+      handleRemoveFavorite(item);
+    } else {
+      handleAddFavorite(item);
+    }
+  };
+
+  // Função para lidar com cliques em itens avaliados
+  const handleRatingClick = (newRating) => {
+    if (!user) {
+      setShowModal(true);
+      setTextMessage('avaliar o jogo');
+      return;
+    }
+    if (ratings[item.id] === newRating) {
+      handleRemoveRatings(item);
+      setIsRating(0);
+    } else {
+      handleAddRatings({ ...item, rating: newRating });
+      setIsRating(newRating);
+    }
+  };
+
+  return {
+    isFavorite,
+    isRating,
+    showModal,
+    setShowModal,
+    textMessage,
+    setTextMessage,
+    handleFavoritClick,
+    handleRatingClick,
+  };
+};
+
+export default useCard;
